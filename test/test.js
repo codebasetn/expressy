37daeddf8859658de0b182f5e4b6536532a2075a
codebasetn <codebasetn@gmail.com>
2018-09-25 13:59:13 +0200

    Test purposes

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function() {
+ it('respond with hello codebasetn to expressy', function(done) {
+
+ //navigate to root and check the the response is "hello world"
+ request(app).get('/').expect('hello codebasetn to expressy!', done);
+ });
+});
