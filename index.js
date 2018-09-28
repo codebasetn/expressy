023719a4bcef504f4deecc9cc0077c1f7f50a8a6
codebasetn <codebasetn@gmail.com>
2018-09-28 14:21:55 +0200

    Travis integration

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
 
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
-console.log('Listening on port 3000...') 
+console.log('Listening on port 3000...') ;
 
 module.exports = app;
